97280fdea8bacb0bf7e8b7030cedea612ba0e5fb
EmelKesten <kestenemel@gmail.com>
2022-07-01 20:15:32 +0200

    commit

--- react-exs/src/App.js
+++ react-exs/src/App.js
@@ -1,12 +1,9 @@
  import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
   return (
     <div className="App">
-      <p>{count}</p>
-      <button onClick={() => setCount(count + 1)}>increase</button>
-      <button onClick={() => setCount(count - 1)}>decrease</button>
+      
     </div>
   );
 }
