2d5cdeb65c576f90343454ea5a5d7a21cedb13f4
EmelKesten <kestenemel@gmail.com>
2022-07-13 16:45:38 +0200

    commit

--- react-exs/src/App.js
+++ react-exs/src/App.js
@@ -1,9 +1,41 @@
+import React, {useEffect, useState} from 'react';
  import './App.css';
 
+
+const Header = (props) => {
+  const {name, counter} = props;
+  return (
+    <div className="header">
+      <h1>Hi {name}</h1>
+      <h2>Counter: {counter}</h2>
+    </div>
+  );
+}
+
+
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setCounter(5);
+      setLoading(false);
+    }, 2000);
+  }, []);
+
+  const increaseCounter = () => { setCounter(counter + 1) }
+  const decreaseCounter = () => { setCounter(counter - 1) }
+
   return (
     <div className="App">
-      
+      {loading ? <h1>Loading...</h1> :
+      <>
+      <Header name="React" counter={counter} />
+      <button onClick={increaseCounter}>Increase</button>
+      <button onClick={decreaseCounter}>Decrease</button>
+    </>
+    }
     </div>
   );
 }
