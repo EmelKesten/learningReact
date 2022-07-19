36955a2c6261ee414861c60ea12ed06d1760c612
EmelKesten <kestenemel@gmail.com>
2022-07-19 20:57:51 +0200

    commit

--- react-exs/src/App.js
+++ react-exs/src/App.js
@@ -1,40 +1,14 @@
 import React, {useEffect, useState} from 'react';
- import './App.css';
-
-
-const Header = (props) => {
-  const {name, counter} = props;
-  return (
-    <div className="header">
-      <h1>Hi {name}</h1>
-      <h2>Counter: {counter}</h2>
-    </div>
-  );
-}
+import './App.css';
+import './Movies.css';
+import axios from 'axios';
+import TopMovies from './top-movies/Index.js';
 
 
 function App() {
-  const [counter, setCounter] = useState(0);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 2000);
-  }, []);
-
-  const increaseCounter = () => { setCounter(counter + 1) }
-  const decreaseCounter = () => { setCounter(counter - 1) }
-
   return (
     <div className="App">
-      {loading ? <h1>Loading...</h1> :
-      <>
-      <Header name="React" counter={counter} />
-      <button onClick={increaseCounter}>Increase</button>
-      <button onClick={decreaseCounter}>Decrease</button>
-    </>
-    }
+      <TopMovies />
     </div>
   );
 }
